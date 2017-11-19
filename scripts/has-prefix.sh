16ff7cb57c3244c7a28f46b52350863ce1b8dbe6
Omri Lotan <omri@hamadgera.com>
2017-11-19 12:37:48 +0200

    Add actual package name to has-prefix error message

--- scripts/has-prefix.sh
+++ scripts/has-prefix.sh
@@ -7,7 +7,7 @@ prefix=$1
 package_name=$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[:space:]')
 
 if [[ $package_name != $1* ]]; then
-  echo "The package name doesn't start with $1. You can not proceed"
+  echo "The package name doesn't start with $1 (${package_name}). You can not proceed"
   exit 1
 fi
 
