c44f04fa60866ceb828590e9743c5d57b4d28855
omrilotan <omri@hamadgera.com>
2022-01-09 15:41:32 +0000

    Replace colors with chalk

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('colors');
+const { green } = require('chalk')
 const ls = require('./lib/ls');
 const program = require('./lib/program');
 const execute = require('./lib/execute');
@@ -10,7 +10,7 @@ ls(`${__dirname}/scripts`)
     .then(scripts => program(scripts))
     .then(([script, args]) => execute(`${__dirname}/scripts/${script} ${args.join(' ')}`))
     .then((message, exitCode) => {
-        console.log((message || '').toString().green.bold);
+        console.log(green.bold((message || '').toString()));
         process.exit(exitCode);
     })
     .catch(error => {
